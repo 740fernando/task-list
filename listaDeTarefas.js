2e6f0115223e2b827b254625cf142a7790aa470d
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-11-09 09:57:11 -0300

    Implementado 'remover tarefas'

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,4 +1,5 @@
-const criarTarefa = (evento) =>{
+(()=>{
+    const criarTarefa = (evento) =>{
     //evento passa um objeto com as informacoes
     evento.preventDefault() // prevençao de evento padrao
     
@@ -9,31 +10,85 @@ const criarTarefa = (evento) =>{
     
     const tarefa = document.createElement('li') // cria um elemento do dom
 
-    tarefa.classList.add('task');
+    tarefa.classList.add('task'); // adiciona um classe dentro de uma classe ja existente
 
     const conteudo = `<p class="content">${valor}</p>`;
     
     tarefa.innerHTML=conteudo; //innerHtml- cria o elemento no dom
 
-    lista.appendChild(tarefa);
+    tarefa.appendChild(BotaoConclui()); // coloca botao dentro da li
+    tarefa.appendChild(BotaoDeleta());
+    lista.appendChild(tarefa);// coloca tarefa dentro da li
     input.value=''//limpa o input após ser clicado
 
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');// percorre o dom e seleciona o parametro pasado
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', criarTarefa);
 
+//BotaoConclui é um componenet, foi utilizado letra maiuscula para diferenciar da funcao
+const BotaoConclui = () =>{
+    const botaoConclui = document.createElement('button')
 
-// DOM - REPRESENTACAO DO HTML EM FORMAS DE NÓS
+    botaoConclui.classList.add('check-button'); // adicona classe css
+    botaoConclui.innerText= 'concluir';
 
-//EventListener= Elemento que recebe o evento / O que vai acontecer quando o evento disparar - Precisamos do tipo do evento, do elemento que vai receber o evento e o que vai acontecer quando o evento for disparado
+    botaoConclui.addEventListener('click',concluirTarefa)
 
-// Sempre que eu estou trabalhando com eventos, é necessario saber o evento que está clicando
-// addEventListerner que passa um objeto como parametro da funcao
+    return botaoConclui;
 
+}
+const concluirTarefa = (evento) =>{
+    const botaoConclui = evento.target// quero saber o alvo do evento, ou seja, quando eu clicar, eu quero saber onde eu cliquei
 
+    const tarefaCompleta = botaoConclui.parentElement// tarefaCompleta recebe o pai da funcao botaoConclui, ou seja, a <li>
+
+    tarefaCompleta.classList.toggle('done') // toggle- executa a classe css(efeito de riscado) a partir do momento que é clicado no botao
+}
+
+const BotaoDeleta = ()=>{
+    
+    const botaoDeleta = document.createElement('button');
+    botaoDeleta.innerText = 'deletar';
+    botaoDeleta.addEventListener('click', deletarTarefa)
+    return botaoDeleta;
+
+}
+const deletarTarefa= (evento) =>{
+    const botaoDeleta = evento.target;
+    
+    const tarefaCompleta= botaoDeleta.parentElement;
+
+    tarefaCompleta.remove();
+
+    return botaoDeleta
+}
+})()
 /*
+
+Vamos utilizar a “IIFE”, ou Immediately Invoked Function Expression ou “Função de Invocação Imediata”.
+ Vamos colocar todo esse nosso arquivo dentro de um escopo de uma função. Então, a primeira coisa que
+ vamos fazer é colocar dois parênteses, um no começo e um no final do nosso arquivo, e a nossa função já 
+ está dentro desses parênteses. Agora, vamos utilizar uma função anônima para executar.
+   Essa função anônima vai receber uma arrow function e essa arrow function tem duas chaves, 
+ uma vai no começo e uma vai no final, antes do parênteses. Vou dar um “Enter” para quebrar, 
+ e tudo certo, nós colocamos dentro do escopo da função.
+
+
+Através da variável botaoConclui capturamos qual elemento foi clicado, 
+depois utilizamos a propriedade parentElement para subir um nó na árvore, 
+por fim colocamos o método toggle que vai adicionar a classe done quando clicarmos no botão
+
+DOM - REPRESENTACAO DO HTML EM FORMAS DE NÓS
+
+EventListener= Elemento que recebe o evento / O que vai acontecer quando o evento disparar - Precisamos do tipo do evento, do elemento que vai receber o evento e o que vai acontecer quando o evento for disparado
+
+Sempre que eu estou trabalhando com eventos, é necessario saber o evento que está clicando
+addEventListerner que passa um objeto como parametro da funcao
+
+
+
 O HTML5 foi criado pensando na extensibilidade dos dados que precisam ser associados a um determinado
  elemento mas não necessariamente tem um significado definido.  Atributos data-* nos permite armazenar
   informações extras em elementos HTML padrões e semânticos, sem a necessidades de hacks como classList, atributos fora do padrão, propriedades extras no DOM ou o método depreciado setUserData.
